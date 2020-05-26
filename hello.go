93d5802ad37e87beeea2df94e704389d944bdb3b
Edgar <s985432@gmail.com>
2020-05-26 17:36:21 +0800

    05/26 update
    05/26 update

--- hello.go
+++ hello.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/hakumamatata/hello/morestrings"
 	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
-	fmt.Printf("hello, world\n")
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Printf("hello, my name is edgar\n")
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
